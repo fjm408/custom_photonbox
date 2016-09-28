ca470c7070919ed40ae3fac10703e51aba9dc472
Shawn Ho <hos@vmware.com>
2016-09-28 11:37:37 +0800

    add auto_correct flag to photon-vagrant_template.rb

--- scripts/photon-vagrantfile_template.rb
+++ scripts/photon-vagrantfile_template.rb
@@ -4,7 +4,7 @@ Vagrant.configure('2') do |config|
 
     config.vm.synced_folder '.', '/vagrant', disabled: true
     config.nfs.functional = false
-    config.vm.network "forwarded_port", guest: 2375, host: 2375
+    config.vm.network "forwarded_port", guest: 2375, host: 2375, auto_correct:true
 
     config.vm.provider :vmware_fusion do |v|
         v.gui = false
